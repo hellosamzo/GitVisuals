26fdac99f4c2b05b6c34917d0cb98f08a4d91914
Sam <samuelmatthews96@gmail.com>
2020-04-11 17:58:46 +0100

    Now read email from config file
    Setup config file reading and assigned the email value to come from config.yaml

--- main.go
+++ main.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"flag"
+	"fmt"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig() // Find and read the config file
+
+	if err != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
+	var email string = viper.GetString("email")
+	//var githubRepos = viper.GetStringMap("githubRepos")
+
+	print(email)
 	var dir string
-	var email string
 	flag.StringVar(&dir, "add", "", "add a new directory to scan for Git repositories")
-	flag.StringVar(&email, "email", "email@gmail.com", "the emai to scan")
+	flag.StringVar(&email, "email", email, "the email to scan")
 	flag.Parse()
 
 	if dir != "" {
